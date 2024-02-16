2295efa2b4a954393a3b45285d702806d0ddc86e
ArthurDaCosta <156153011+ArthurDaCosta@users.noreply.github.com>
2024-02-16 11:45:25 -0300

    execicio 6

--- Desafio1/exercicio5/teste/MinhaClasseAvancadoTest.php
+++ Desafio1/exercicio5/teste/MinhaClasseAvancadoTest.php
@@ -8,30 +8,30 @@ class MinhaClasseAvancadoTest extends TestCase
     public function testSomar()
     {
         $minhaClasse = new MinhaClasseAvancado();
-        $this->assertEquals(3                                           , $minhaClasse->somar(1, 2));
-        $this->assertEquals(0                                           , $minhaClasse->somar(0, 0));
-        $this->assertEquals(-1                                          , $minhaClasse->somar(-1, 0));
+        $this->assertEquals(3   , $minhaClasse->somar(1, 2));   //Soma de números positivos
+        $this->assertEquals(0   , $minhaClasse->somar(0, 0));   //Soma de zeros
+        $this->assertEquals(-1  , $minhaClasse->somar(-1, 0));  //Soma de número negativo com zero
     }
 
     public function testSomarArgumentosInválidos()
     {
         $minhaClasse = new MinhaClasseAvancado();
-        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->somar('a', 2));
-        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->somar(2, null));
+        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->somar('a', 2));  //Soma de número com string
+        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->somar(2, null)); //Soma de número com null
     }
 
     public function testSubtrair()
     {
         $minhaClasse = new MinhaClasseAvancado();
-        $this->assertEquals(1, $minhaClasse->subtrair(3, 2));
-        $this->assertEquals(0, $minhaClasse->subtrair(0, 0));
-        $this->assertEquals(-1, $minhaClasse->subtrair(1, 2));
+        $this->assertEquals(1   , $minhaClasse->subtrair(3, 2));  //Subtração de números positivos
+        $this->assertEquals(0   , $minhaClasse->subtrair(0, 0));  //Subtração de zeros
+        $this->assertEquals(-1  , $minhaClasse->subtrair(1, 2));  //Subtração de número positivo com resultado negativo
     }
 
     public function testSubtrairArgumentosInválidos()
     {
         $minhaClasse = new MinhaClasseAvancado();
-        $this->assertEquals('Um dos valores informados não é um número', $minhaClasse->subtrair('a', 2));
-        $this->assertEquals('Um dos valores informados não é um número', $minhaClasse->subtrair(2, null));
+        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->subtrair('a', 2));  //Subtração de número com string
+        $this->assertEquals('Um dos valores informados não é um número' , $minhaClasse->subtrair(2, null)); //Subtração de número com null
     }
 }
